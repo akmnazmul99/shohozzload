9dbcbf782ffdbac8e64a94152bfe87cc9822d95a
A. K. M. NAZMUL ISLAM <nazmulislamredoy@gmail.com>
2016-10-31 12:42:15 +0600

    initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+//$(document).ready(function() {
+//    $('.bxslider').bxSlider();
+//});
+$(document).ready(function() {
+    wow = new WOW(
+            {
+                animateClass: 'animated',
+                offset: 100,
+            }
+    );
+    wow.init();
+});
